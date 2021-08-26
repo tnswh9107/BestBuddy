6f0741d7fe21559f121579ee06b9c2dceba8aee0
hoyaok2 <tnswh9107@gmail.com>
2021-08-27 04:31:07 +0900

    FireBase 수정중

---
+++ app/src/main/java/com/hoyaok2/bestbuddy/Mypage_MyFCMService.java
@@ -0,0 +1,68 @@
+package com.hoyaok2.bestbuddy;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+//서비스 컴포넌트는 4개컴포넌트이기 떄문에 매니페스트에 등록해야한다.
+public class Mypage_MyFCMService extends FirebaseMessagingService {
+    //FCM 메세지를 수신하면 자동으로 발동 하는 콜백 메소드
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        //메소드 발동 확인하기 위해 Log 기록 표시 - Activity가 없으니 Toast는 사용 하지 않는다.
+        Log.i("TAG", "onMessageReceived...");
+
+        //PUSH 서비스는 앱이 꺼져있는 상태에서 서버가 강제로 Client 에게
+        //정보를 보내는 것이기에 알림을 이요해서 정보를 전달한다.
+        //버전업이 되면서 앱이 꺼져있으면 무조건 자동으로 Nofication을 만들어 보여줌
+
+        //알림을 제어하고 싶다면 직접 알림객체를 이곳에서 설계
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder builder = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new  NotificationChannel("ch1","push Chennel",NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+
+            builder = new NotificationCompat.Builder(this,"ch1");
+        }else{
+            builder = new NotificationCompat.Builder(this,"");
+
+        }
+
+        //FCM서버로 부터 전달되어온 알림제목, 알림메세지를 얻어오기
+        //onMessageReceied() 메소드의 파라미터 전달되어온 RemoteMessage remoteMessage(원격메세지)가 정보를 가지고있음
+        String notiTitle = "title";     //알림정보가 없을때 기본제목
+        String notiText = "message";    //알림정보가 없을때 기본글씨
+
+        if(remoteMessage.getNotification()!=null){
+            notiTitle = remoteMessage.getNotification().getTitle();
+            notiText = remoteMessage.getNotification().getBody();
+        }
+
+
+        //알림의 주요 설정들
+        builder.setSmallIcon(R.drawable.ic_fcm_noti); //상태 표시줄에 보이는 아이콘
+        builder.setContentTitle(notiTitle);  //확장상태바(알림창) 의 제목 글씨
+        builder.setContentText(notiText);   //확장상태바(알림창) 의 메세지 글씨
+
+        //알림객체 생성 및 공지[알림보이기]
+        Notification notification = builder.build();
+        notificationManager.notify(11,notification);
+
+
+    }
+}
+
--- app/src/main/java/com/hoyaok2/bestbuddy/Play_Show.java
+++ app/src/main/java/com/hoyaok2/bestbuddy/Play_Show.java
@@ -1,21 +1,33 @@
 package com.hoyaok2.bestbuddy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class Play_Show extends AppCompatActivity {
 
+//    FCM작업할 것 3단계
+//    1.FCM SDK library 추가 [Firebase console 앱등록]
+//    2. FCM서버에서 보낸 메세지를 수신하기 위한 Service클래스 작성
+//    3. 앱을 FCM서버에 등록하여 토큰 받기
 
-    ImageView showiv;
-    TextView showtitle,showsub,showprice,showphone;
+//    먼저저
+
+   ImageView showiv;
+    TextView showtitle, showsub, showprice, showphone;
 
 
     @Override
@@ -38,15 +50,13 @@ public class Play_Show extends AppCompatActivity {
         String phone = intent.getStringExtra("phone");
 
 
+        String imgUrl = "http://tnswh9107.dothome.co.kr/BuddyPlay/" + pic;
 
-
-        String imgUrl = "http://tnswh9107.dothome.co.kr/BuddyPlay/"+pic;
-
-        Log.d("test",imgUrl);
+        Log.d("test", imgUrl);
 
 //        Log.d("쪼매니 혜니",)
 
-        if (imgUrl==null) Glide.with(this).load(R.drawable.noimage).into(showiv);
+        if (imgUrl == null) Glide.with(this).load(R.drawable.noimage).into(showiv);
         else Glide.with(this).load(imgUrl).into(showiv);
 
         showtitle.setText(title);
@@ -54,12 +64,33 @@ public class Play_Show extends AppCompatActivity {
         showprice.setText(price);
         showphone.setText(phone);
 
-        if (Build.VERSION.SDK_INT>Build.VERSION_CODES.LOLLIPOP){
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
             showiv.setTransitionName("PlayShow");
         }
 
 
+    }
 
+    public void Play_FCMmessage(View view) {
+        FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
+            @Override
+            public void onComplete(@NonNull Task<String> task) {
+                if (!task.isSuccessful()) {
+                    Toast.makeText(Play_Show.this, "앱 등록 실패", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                String token = task.getResult(); //토큰값 결과 얻어오기
+
+                //원래는 token값을 웹서버에 보내서 DB에 저장해 놓아야함
+                //추후 token 값을 dothme 값은 웹서버에 사용하기 위해 Log로 출력해보기
+                //토스트와 Logcat모두 출력
+                Toast.makeText(Play_Show.this, "" + token, Toast.LENGTH_SHORT).show();
+                Log.i("TOKEN", token);
+
+            }
+
+        });
 
     }
 }
